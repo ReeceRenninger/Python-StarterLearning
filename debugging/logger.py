11d081b309399f198f859aa5cd0665b0e8b53688
ReeceRenninger <renningerreece@gmail.com>
2023-09-01 13:52:00 -0700

    learning how to write to a file for logging instead of to the console, this will create a new file in the directory you are in and print your logging messages you have set up

--- debugging/logger.py
+++ debugging/logger.py
@@ -2,8 +2,8 @@
 # line 3 & 4 is the basic configuration for logging
 #! DO NOT NAME YOUR FILE logging.py, it will cause an error when you try to import the logging module
 import logging
-#
-logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+# if you want to write your log to a file, you can add a filename argument before the LEVEL argument to the basicConfig method, i.e. filename='programLog.txt'
+logging.basicConfig(filename= 'programlog.txt', level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
 # The different levels of logging are: 
 # DEBUG, INFO, WARNING, ERROR, and CRITICAL (in order of least to most severe), so calling CRITICAL in the disable method will disable ALL LOGGING messages
 # logging.disable(logging.CRITICAL) # this will disable all logging messages
@@ -17,6 +17,7 @@ def factorial(n):
     total *= i
     logging.debug(f'''i is {i}, total is {total}''')
   return total
+
 print(factorial(5))
 
 logging.debug('End of program')
