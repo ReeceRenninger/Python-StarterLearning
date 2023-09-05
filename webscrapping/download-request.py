f8b69d3fa04624a03ca4ee039a643280d0bfffc3
ReeceRenninger <renningerreece@gmail.com>
2023-09-05 12:22:40 -0700

    requests module overview of accessing a file and downloading and then being able to get information from the file and writing it to my own file if needed

--- webscrapping/download-request.py
+++ webscrapping/download-request.py
@@ -4,3 +4,16 @@ import requests
 
 res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
 print(res.status_code) # 200 means everything is ok
+
+print(len(res.text)) # 178978
+
+res.raise_for_status() # if there is an error, it will raise an exception
+
+# badRes = requests.get('https://automatetheboringstuff.com/files/fake.txt')
+# badRes.raise_for_status() # raise an exception
+
+# you need to open in binary mode to maintain unicode encoding
+playFile = open('RomeoAndJuliet.txt', 'wb') # wb = write binary
+
+for chunk in res.iter_content(100000): # 100000 bytes
+    playFile.write(chunk)
