2b38c40554e1e608599c9bf3d02e8a5555c65725
ReeceRenninger <renningerreece@gmail.com>
2023-09-08 18:04:52 -0700

    issue with pyzmail and python version, had to use pip install pyzmail36 instead of the standard install

---
+++ emails/check-inbox.py
@@ -0,0 +1,5 @@
+# IMAP internet message access protocol
+# the built in imap module is very difficult to use so we will use third party module called imapclient and pyzmail
+#! pyzmail has some issues with specific versions of python so you may need to use pip install pyzmail36 if you are using python 3.6+
+
+
