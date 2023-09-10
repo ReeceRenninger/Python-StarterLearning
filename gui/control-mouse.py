675e7316bd8fe6f400946d988f32dc2cf20b809f
ReeceRenninger <renningerreece@gmail.com>
2023-09-09 19:32:55 -0700

    unable to get pyautogui working in my WSL, still getting same error,payautogui may require an older version of python to be installed

--- gui/control-mouse.py
+++ gui/control-mouse.py
@@ -2,6 +2,8 @@
 # using pyautogui module
 # docs can be found at https://pyautogui.readthedocs.org
 
+#! failsafe built into pyautogui to stop the program if you quickly move the mouse to the upper left corner of the screen
+
 import pyautogui
 import os
 
@@ -10,3 +12,30 @@ os.environ['DISPLAY'] = ':0' # this is for linux so that it knows which display 
 width, height = pyautogui.size() # returns the screen resolution
 print(width)
 print(height)
+
+# move the mouse to a specific location
+pyautogui.moveTo(10, 10) # move the mouse to the top left corner of the screen
+pyautogui.moveTo(10, 10, duration=1.5) # move the mouse to the top left corner of the screen over 1.5 seconds
+
+# move the mouse relative to its current position
+pyautogui.moveRel(200, 0, duration=2) # move the mouse 200 pixels to the right of its current position over 2 seconds
+
+# get mouse position
+print(pyautogui.position()) # returns a tuple of the mouse's current position
+
+pyautogui.displayMousePosition() # displays the mouse position in a pop up window
+
+# use the click() method to click the mouse
+pyautogui.click(339, 38) # click the mouse at the given coordinates
+
+# use the rightClick() method to right click the mouse
+pyautogui.rightClick(339, 38) # right click the mouse at the given coordinates
+
+# use the doubleClick() method to double click the mouse
+pyautogui.doubleClick(339, 38) # double click the mouse at the given coordinates
+
+# use the dragTo() method to drag the mouse
+pyautogui.dragTo(339, 38, duration=2) # drag the mouse to the given coordinates over 2 seconds
+
+# use the dragRel() method to drag the mouse relative to its current position
+pyautogui.dragRel(200, 0, duration=2) # drag the mouse 200 pixels to the right of its current position over 2 seconds
